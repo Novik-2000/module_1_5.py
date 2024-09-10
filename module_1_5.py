2338c4f06a47935fcc8d14c899ca50ee3dfc100a
Novik-2000 <lyubasha.novikova.2000@mail.ru>
2024-09-10 22:37:28 +0300

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,8 @@
+immutable_var = 1,2,3,'a','s'
+print(immutable_var)
+#immutable_var [0] = 45  кортеж не поддерживает обращение по элементам
+
+mutable_list = [1,2,5,'r','o']
+print(mutable_list)
+mutable_list [0]=8
+print(mutable_list)
